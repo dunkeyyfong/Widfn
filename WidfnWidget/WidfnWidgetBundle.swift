f047c024e66c29616e7d078e228931bfe188a942
dunkeyyfong <phongtrong865@gmail.com>
2024-09-10 22:13:13 +0700

    First upload

---
+++ WidfnWidget/WidfnWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidfnWidgetBundle.swift
+//  WidfnWidget
+//
+//  Created by dunkeyyfong on 10/09/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidfnWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WidfnWidget()
+    }
+}

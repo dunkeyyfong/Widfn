e68c7d19c25ee5c4acf4daf496626e5a26739aca
dunkeyyfong <phongtrong865@gmail.com>
2024-09-10 22:09:11 +0700

    Initial Commit

---
+++ Widfn/WidfnApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidfnApp.swift
+//  Widfn
+//
+//  Created by dunkeyyfong on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WidfnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
